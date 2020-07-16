40cf5a9625969e0a41bb63fedee074a8e4577500
richard-mcghee <richardjmcghee@yahoo.com>
2020-07-16 01:57:56 -0400

    wrote the additional article and verified it's working. That should be it for this project.

--- components/Article.js
+++ components/Article.js
@@ -85,6 +85,15 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'A Test Title for Testing',
+    date: 'July 16, 2020',
+    firstParagraph: `Right, about that writing a new article thing `,
+
+    secondParagraph: `Indeed, more of this.`,
+
+    thirdParagraph: `Yes, and a final paragraph.`
   }
 ];
 
